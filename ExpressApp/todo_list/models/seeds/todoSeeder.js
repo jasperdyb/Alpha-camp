12ed2a7c4f50f03f9c2dde3bfbf323f36dfb3aa7
Jasper Chen <jasperdyb@gmail.com>
2020-02-26 13:48:40 +0800

    Use seeder

---
+++ ExpressApp/todo_list/models/seeds/todoSeeder.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Todo = require('../todo')
+
+mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true, useUnifiedTopology: true })
+
+db = mongoose.connection
+
+db.on('error', () => {
+  console.log('Database connection error')
+})
+
+db.once('open', () => {
+  console.log('database connected.')
+
+  for (var i = 0; i < 10; i++) {
+    Todo.create({ name: 'name-' + i })
+  }
+
+  console.log('done')
+})
