67b725687642acda5cfc3184c05b00cd8f222f9e
Jasper Chen <jasperdyb@gmail.com>
2020-02-26 13:19:37 +0800

    connect to db

---
+++ ExpressApp/todo_list/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')                    // 載入 mongoose
+
+mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true, useUnifiedTopology: true })   // 設定連線到 mongoDB
+
+// mongoose 連線後透過 mongoose.connection 拿到 Connection 的物件
+const db = mongoose.connection
+
+// 載入 todo model
+const Todo = require('./models/todo')
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.get('/', (req, res) => {
+  res.send('hello world!')
+})
+
+app.listen(3000, () => {
+  console.log('App is running!')
+})
